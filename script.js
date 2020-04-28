152ed776ec76404ac28bafd41a418b807a9719ec
Nivolving <nivethapartheeban@gmail.com>
2020-04-27 23:07:44 -0700

    updated game file

--- script.js
+++ script.js
@@ -1,48 +1,103 @@
-/*Set of questions*/
-var gameQuestions = [
- {question: "What are JavaScript Data Types?",
-  answerChoices:['Boolean','Integer','String','All of the above'],
-  corretAnswer:"All of the above"
-},
-{question: "What is === operator?",
-answerChoices:['equality operator','not equality','addition','none of the above'],
-corretAnswer:"equality operator"
-},
-{question: "What are all the types of Pop up boxes available in JavaScript?",
-answerChoices:['Alert','change','hello','printf'],
-corretAnswer:'Alert'
-},
-{question: "What should appear at the very end of your JavaScript?",
-answerChoices:['The </script>','The <script>','The END statement','None of the above'],
-corretAnswer:'The </script>'
-},
-{question: "Inside which HTML element do we put the JavaScript?",
-answerChoices:['<javascript>','<script>','<scripting>','<js>'],
-corretAnswer:'<script>'
-}];
+var quizBeginEl = document.getElementById("quizBegin");
+var startButtonEl = document.getElementById("quiz-start")
+var quizEl = document.getElementById('quiz');
+var quizEndEl = document.getElementById('quizEnd');
+var questionEl = document.getElementById("question");
+var timeEl = document.getElementById("timer");
+var submitDivEl = document.getElementById("submit-initials");
+var answerLabeEl0 = document.getElementById("option0");
+//console.log(answerLabeEl0);
+var answerLabeEl1 = document.getElementById("option1");
+//console.log(answerLabeEl1);
+var answerLabeEl2 = document.getElementById("option2");
+//console.log(answerLabeEl2);
+var answerLabeEl3 = document.getElementById("option3");
+//console.log(answerLabeEl3);
+var index = 0;
+//var leftOverTime = 0;
+var quizTime = 75;
+var score = 0;
+var result = document.createElement("p");
+result.setAttribute('style', 'font-family:Impact,Charcoal,sans-serif')
 
-document.getElementById("button-start").addEventListener('click', function(){
+/* function to begin the quiz : adding EventListener:"Click" to get the event started */
 
-  startQuiz();
+startButtonEl.addEventListener('click', function () {
+  quizBeginEl.setAttribute("class", 'd-none');
+  quizEl.setAttribute("class", "container");
+  setTime();
+  startQuiz(index);
   
 
+  answerLabeEl0.addEventListener("click", function () {
+    validateInput(answerLabeEl0);
+  });
+  answerLabeEl1.addEventListener("click", function () {
+    validateInput(answerLabeEl1);
+  });
+  answerLabeEl2.addEventListener("click", function () {
+    validateInput(answerLabeEl2);
+  });
+  answerLabeEl3.addEventListener("click", function () {
+    validateInput(answerLabeEl3);
+  });
 
+  document.getElementById("nextbutton").addEventListener('click', function () {
+    ["option0", "option1", "option2", "option3"].forEach(function (id) {
+      document.getElementById(id).checked = false;
+    });
+    index++;
+    if(index >= gameQuestions.length){
+      clearTimeout(quizTime);
+      quizEl.setAttribute("class", "d-none");
+      submitDivEl.setAttribute("class", "container");
+    }
+    else{
+      startQuiz(index);
+    }
+    result.innerHTML = " ";
+    quizEl.append(result);
+  
+  });
 });
 
+/* As i have created an array of game questions and answers as an nested array inside my gamequestions
+created the below for loop to get the elements for my list option*/
+function startQuiz(qnIndex) {
+  questionEl.innerHTML = gameQuestions[qnIndex].question;
+  for (var i = 0; i < gameQuestions[qnIndex].answerChoices.length; i++) /**runs the length of my answerChoices array*/ {
+    //console.log(gameQuestions[qnIndex].answerChoices);
+    document.getElementById('option' + i).setAttribute("value", gameQuestions[qnIndex].answerChoices[i]);
+    document.getElementById('answerchoices' + i).innerHTML = gameQuestions[qnIndex].answerChoices[i];
+    //console.log(gameQuestions[qnIndex].answerChoices[i]);
 
-function startQuiz(gameQuestions)
-{
-  var element = document.createElement('p');
-  element.textContent(gameQuestions[0]);
-  
-
+  }
 }
 
-
-function startTimer(){
-
+function validateInput(ansIndex) {
+  var userInput = ansIndex.getAttribute("value");
+  if (userInput === gameQuestions[index].correctAnswer) {
+    //var hrTag = document.createElement("hr");
+    result.innerHTML = "Correct";
+    quizEl.append(result);
+  }
+  else {
+    result.innerHTML = "Incorrect";
+    quizTime = quizTime - 10;
+    quizEl.append(result);
+  }
 }
 
-function quizScore(){
+function setTime() {
+  var timerInterval = setInterval(function () {
+    quizTime--;
+    timeEl.value = quizTime;
+    console.log(quizTime)
 
+    if (quizTime === 0) {
+      clearInterval(timerInterval);
+      //gameOver();
+    }
+
+  }, 1000);
 }
