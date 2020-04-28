172c2be1409479ff1866ea0bd663035ccfdc787d
Nivolving <nivethapartheeban@gmail.com>
2020-04-27 23:09:21 -0700

    question file

---
+++ questions.js
@@ -0,0 +1,21 @@
+var gameQuestions = [
+    {question: "1 . What are JavaScript Data Types?",
+     answerChoices:['Boolean','Integer','String','All of the above'],
+     correctAnswer:"All of the above"
+   },
+   {question: "2. What is === operator?",
+   answerChoices:['equality operator','not equality','addition','none of the above'],
+   correctAnswer:"equality operator"
+   },
+   {question: "3. What are all the types of Pop up boxes available in JavaScript?",
+   answerChoices:['Alert','change','hello','printf'],
+   correctAnswer:'Alert'
+   },
+   {question: "4. how to declare a variable?",
+   answerChoices:['var','int','const','let'],
+   correctAnswer:'var'
+   },
+   {question: "5. which one of the following is a loop?",
+   answerChoices:['for','if','because','yes'],
+   correctAnswer:'for'
+   }];
